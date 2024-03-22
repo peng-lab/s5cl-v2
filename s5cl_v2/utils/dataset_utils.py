0176eab63fc9e89bc95dec6338cb9686db838332
Manuel Tran <95863934+manuel-tran@users.noreply.github.com>
2024-03-22 17:36:52 +0100

    Update dataset_utils.py

--- s5cl_v2/utils/dataset_utils.py
+++ s5cl_v2/utils/dataset_utils.py
@@ -9,39 +9,6 @@ from PIL import Image
 from torch.utils.data import Dataset
 
 
-def make_dataset(dataset, images_per_class=5):
-    labeled_indices = []
-
-    # randomly sample labeled indices
-    for i in range(0, 9):
-        indices = random.sample(
-            np.where(np.array(dataset.targets) == i)[0].tolist(),
-            images_per_class
-        )
-        labeled_indices = labeled_indices + indices
-
-    # recover the corresponding labels
-    labeled_targets = [dataset.targets[i] for i in labeled_indices]
-
-    # create the labeled subset
-    labeled_dataset = torch.utils.data.Subset(dataset, labeled_indices)
-    labeled_dataset = DatasetFromSubset(labeled_dataset, transform=None)
-
-    # length of original dataset
-    total_indices = list(range(0, 100000))
-
-    # indices of unlabeled dataset
-    unlabeled_indices = [
-        index for index in total_indices if index not in labeled_indices
-    ]
-
-    # create the unlabeled subset
-    unlabeled_dataset = torch.utils.data.Subset(dataset, unlabeled_indices)
-    unlabeled_dataset = DatasetFromSubset(unlabeled_dataset, transform=None)
-
-    return labeled_targets, labeled_dataset, unlabeled_dataset
-
-
 class DatasetFromSubset(Dataset):
     def __init__(self, subset, transform=None):
         self.subset = subset
@@ -109,3 +76,36 @@ class COAD(data_utils.Dataset):
         image = self.transform(image)
         label = 0
         return image, label
+
+
+def make_dataset(dataset, images_per_class=5):
+    labeled_indices = []
+
+    # randomly sample labeled indices
+    for i in range(0, 9):
+        indices = random.sample(
+            np.where(np.array(dataset.targets) == i)[0].tolist(),
+            images_per_class
+        )
+        labeled_indices = labeled_indices + indices
+
+    # recover the corresponding labels
+    labeled_targets = [dataset.targets[i] for i in labeled_indices]
+
+    # create the labeled subset
+    labeled_dataset = torch.utils.data.Subset(dataset, labeled_indices)
+    labeled_dataset = DatasetFromSubset(labeled_dataset, transform=None)
+
+    # length of original dataset
+    total_indices = list(range(0, 100000))
+
+    # indices of unlabeled dataset
+    unlabeled_indices = [
+        index for index in total_indices if index not in labeled_indices
+    ]
+
+    # create the unlabeled subset
+    unlabeled_dataset = torch.utils.data.Subset(dataset, unlabeled_indices)
+    unlabeled_dataset = DatasetFromSubset(unlabeled_dataset, transform=None)
+
+    return labeled_targets, labeled_dataset, unlabeled_dataset
